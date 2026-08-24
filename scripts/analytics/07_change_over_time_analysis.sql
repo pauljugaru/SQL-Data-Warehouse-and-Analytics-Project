47e26c0a8fcb1c0ece33246c48921ff89c4e3178
Jugaru Paul <paul.jugaru@student.unitbv.ro>
2026-08-24 16:58:11 +0300

    Add SQL script for sales performance analysis over time

---
+++ scripts/analytics/07_change_over_time_analysis.sql
@@ -0,0 +1,23 @@
+-- Analyze the sales performance over time
+SELECT 
+	YEAR(order_date) AS order_year,
+	MONTH(order_date) AS order_month,
+	SUM(sales_amount) AS total_sales,
+	COUNT(DISTINCT customer_key) AS total_customers,
+	SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date), MONTH(order_date)  
+ORDER BY YEAR(order_date), MONTH(order_date)  
+
+
+-- Analyze the sales performance over time with DATETRUNC
+SELECT 
+	DATETRUNC(month,order_date) AS order_year,
+	SUM(sales_amount) AS total_sales,
+	COUNT(DISTINCT customer_key) AS total_customers,
+	SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(month,order_date)
+ORDER BY DATETRUNC(month,order_date)
