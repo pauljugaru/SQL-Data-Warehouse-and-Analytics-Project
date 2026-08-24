cfa2af899659e64f52ef7b31bd14b4f43d9476e6
Jugaru Paul <paul.jugaru@student.unitbv.ro>
2026-08-24 16:34:52 +0300

    Add ranking analysis SQL script for sales data

---
+++ scripts/analytics/06_ranking_analysis.sql
@@ -0,0 +1,52 @@
+-- Which 5 products generate the highest revenue?
+SELECT TOP 5
+	p.product_name,
+	SUM(s.sales_amount) AS total_revenue
+FROM gold.fact_sales s
+LEFT JOIN gold.dim_products p
+ON p.product_key = s.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue DESC
+
+
+-- What are the 5 worst-performing products in terms of sales?
+SELECT TOP 5
+	p.product_name,
+	SUM(s.sales_amount) AS total_revenue
+FROM gold.fact_sales s
+LEFT JOIN gold.dim_products p
+ON p.product_key = s.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue
+
+
+-- Find the top 10 customers who have generated the highest revenue
+SELECT TOP 10
+    c.customer_key,
+    c.first_name,
+    c.last_name,
+    SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+    ON c.customer_key = f.customer_key
+GROUP BY 
+    c.customer_key,
+    c.first_name,
+    c.last_name
+ORDER BY total_revenue DESC;
+
+
+-- The 3 customers with the fewest orders placed
+SELECT TOP 3
+    c.customer_key,
+    c.first_name,
+    c.last_name,
+    COUNT(DISTINCT order_number) AS total_orders
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+    ON c.customer_key = f.customer_key
+GROUP BY 
+    c.customer_key,
+    c.first_name,
+    c.last_name
+ORDER BY total_orders ;
